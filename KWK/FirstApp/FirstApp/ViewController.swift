2ee1cf060613bb92f76e5c208a8f66da6e2bc2f5
reddyal <88101624+reddyal@users.noreply.github.com>
2021-07-28 11:39:50 -0400

    1st commit

---
+++ KWK/FirstApp/FirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Aleena Reddy on 7/28/21.
+//  Copyright © 2021 Aleena Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ KWK/TestProject/IntroStoryBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Aleena Reddy on 7/28/21.
+//  Copyright © 2021 Aleena Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("this is a print statement. testing, testing.")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
