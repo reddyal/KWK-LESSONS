af9d13d885ce056acfa5692228a5cd3a503ec34b
reddyal <88101624+reddyal@users.noreply.github.com>
2021-07-29 12:11:50 -0400

    Add files via upload

--- KWK/MiniProject1/MiniProject1/ViewController.swift
+++ KWK/MiniProject1/MiniProject1/ViewController.swift
@@ -9,20 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-var facts = ("i was in a COVID-19 vaccine trial in march", "i've only ever broken one bone", "my dog's name is charlie", "my dog has over 3,000 followers on instagram", "i live in orlando, florida")
+var facts = ["i was in a COVID-19 vaccine trial in march", "i've only ever broken one bone", "my dog's name is charlie", "my dog has over 3,000 followers on instagram", "i live in orlando, florida"]
     
     @IBOutlet weak var appTitle: UILabel!
-    @IBOutlet weak var funFactLabel: UILabel!
     
+    @IBOutlet weak var funFactLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    
-    @IBAction func funFactTapped(_ sender: Any) {let randomIndex = Int.random(in:@..<facts.count)
-        let randomFact = facts(randomIndex)
+    @IBAction func funFactTapped(_ sender: Any) {
+        let randomIndex = Int.random(in: 0..<facts.count)
+        let randomFact = facts[randomIndex]
+   
+    funFactLabel.text = randomFact
     }
-
 }
-
